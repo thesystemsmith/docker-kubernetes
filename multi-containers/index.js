ae1371c8946212826f371c97ae498701e16d038e
abdul.rasheed <abdulrasheed.techy@gmail.com>
2024-06-12 02:08:28 +0530

    docker compose start stop - managing containers

--- multi-containers/index.js
+++ multi-containers/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const redis = require('redis')
+const process = require('process')
 
 const app = express()
 const client = redis.createClient({
@@ -9,6 +10,7 @@ const client = redis.createClient({
 client.set('visits', 0)
 
 app.get('/', (req, res) => {
+    process.exit(0) //crash manually 
     client.get('visits', (err, visits) => {
         res.send('number of visits ' + visits)
         client.set('visits', parseInt(visits)+1)
